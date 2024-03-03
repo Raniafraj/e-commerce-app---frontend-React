f8f697410ed8bd92e3855883ab8639e6ddd3ceb6
Ranyafr <raniafraj2@gmail.com>
2024-03-03 15:05:44 +0100

    e-commerce-app---frontend--React

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+      <div className="descriptionbox-nav-box">Description</div>
+      <div className="descriptionbox-nav-box">Reviews (122)</div>
+
+    </div>
+    <div className="descriptionbox-description">
+        <p>An eCommerce website is an online store that allows customers to purchase products or services from the comfort of their homes. Ecommerce is a shortened term for ‘electronic commerce’.</p>
+    </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
